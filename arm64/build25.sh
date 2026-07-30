bfb609e7554f3a602283871eaeddb65887d7ee78
Arthur97172 <arthur97172@outlook.com>
2026-07-30 21:18:39 +0800

    Update build25.sh

--- arm64/build25.sh
+++ arm64/build25.sh
@@ -313,7 +313,9 @@ fi
 # ============================================
 # 步骤6: 执行 make image
 # ============================================
-make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="files" ROOTFS_PARTSIZE="$ROOTFS_PARTSIZE"
+#make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="files" ROOTFS_PARTSIZE="$ROOTFS_PARTSIZE"
+ROOTFS_PARTSIZE=${ROOTFS_PARTSIZE:-"2048"}
+make image PROFILE=generic PACKAGES="$PACKAGES" FILES="files" ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"

ade148be39e01117e44bf5aa21661ba640ae4bd7
Arthur97172 <arthur97172@outlook.com>
2026-09-02 21:40:36 +0800

    Update build24.sh

--- arm64/build24.sh
+++ arm64/build24.sh
@@ -57,7 +57,7 @@ PACKAGES=""
 
 # [核心系统]
 PACKAGES="base-files libc libgcc uci ubus dropbear logd mtd opkg bash htop curl wget ca-bundle ca-certificates"
-PACKAGES="$PACKAGES -dnsmasq dnsmasq-full firewall4 nftables kmod-nft-offload"
+PACKAGES="$PACKAGES -dnsmasq dnsmasq-full firewall4 nftables kmod-nft-offload nano"
 PACKAGES="$PACKAGES -odhcpd odhcpd-ipv6only"
 PACKAGES="$PACKAGES ip-full ipset iw ppp ppp-mod-pppoe -wpad-basic-mbedtls wpad-openssl luci-proto-ppp luci-proto-ipv6"
 PACKAGES="$PACKAGES kmod-tcp-bbr"
